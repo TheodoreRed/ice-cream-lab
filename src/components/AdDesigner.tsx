c9ee1268c4351d256c01f031514dc4145a9cee33
Theodore Redlon <redlontheo@gmail.com>
2023-11-14 07:27:54 -0500

    Implement interactive ad design and voting features in AdDesigner and Votes components

--- src/components/AdDesigner.tsx
+++ src/components/AdDesigner.tsx
@@ -1,29 +1,95 @@
+import { useState } from "react";
 import "./AdDesigner.css";
 
 const AdDesigner = () => {
+  const [flavor, setFlavor] = useState("Chocolate");
+  const [isDark, setIsDark] = useState(true);
+  const [adFont, setAdFont] = useState(50);
   return (
     <div className="AdDesigner">
       <h2>Ad Designer</h2>
-      <section className="design">
+      <section
+        className="design"
+        style={
+          !isDark
+            ? {
+                backgroundColor: "white",
+                color: "black",
+                border: "3px solid black",
+              }
+            : {}
+        }
+      >
         <p className="vote-for">Vote For</p>
-        <p className="flavor caveat-font">Chocolate</p>
+        <p className="flavor caveat-font" style={{ fontSize: `${adFont}px` }}>
+          {flavor}
+        </p>
       </section>
       <h3>What to Support</h3>
       <section className="flavor-buttons">
-        <button className="chocolate">Chocolate</button>
-        <button className="vanilla">Vanilla</button>
-        <button className="strawberry">Strawberry</button>
+        <button
+          onClick={() => {
+            setFlavor("Chocolate");
+          }}
+          className={flavor === "Chocolate" ? "isActive" : ""}
+        >
+          Chocolate
+        </button>
+        <button
+          onClick={() => {
+            setFlavor("Vanilla");
+          }}
+          className={flavor === "Vanilla" ? "isActive" : ""}
+        >
+          Vanilla
+        </button>
+        <button
+          onClick={() => {
+            setFlavor("Strawberry");
+          }}
+          className={flavor === "Strawberry" ? "isActive" : ""}
+        >
+          Strawberry
+        </button>
       </section>
       <h3>Color Theme</h3>
       <section className="color-theme-buttons">
-        <button className="light">Light</button>
-        <button className="dark">Dark</button>
+        <button
+          onClick={() => {
+            setIsDark(false);
+          }}
+          className={!isDark ? "isActive" : ""}
+        >
+          Light
+        </button>
+        <button
+          onClick={() => {
+            setIsDark(true);
+          }}
+          className={isDark ? "isActive" : ""}
+        >
+          Dark
+        </button>
       </section>
       <h3>Font Size</h3>
       <section className="font-size-section">
-        <button className="down">Down</button>
-        <p className="ad-design-font">56</p>
-        <button className="up">Up</button>
+        <button
+          onClick={() => {
+            setAdFont((prev) => prev - 2);
+          }}
+          className="down"
+        >
+          Down
+        </button>
+        <p className="ad-design-font">{adFont}</p>
+        <button
+          onClick={() => {
+            setAdFont((prev) => prev + 2);
+          }}
+          className="up"
+        >
+          Up
+        </button>
       </section>
     </div>
   );
--- src/components/Votes.tsx
+++ src/components/Votes.tsx
@@ -1,13 +1,44 @@
+import { useState } from "react";
 import "./Votes.css";
 
 const Votes = () => {
+  const [chocVotes, setChocVotes] = useState(0);
+  const [vanVotes, setVanVotes] = useState(0);
+  const [strawVotes, setStrawVotes] = useState(0);
+  const total = chocVotes + vanVotes + strawVotes;
+
+  const getPercent = (votes: number): number => {
+    return total === 0 ? 0 : +((votes / total) * 100).toFixed(2);
+  };
+
   return (
     <div className="Votes">
       <h2>Vote Here</h2>
       <section className="flavor-buttons">
-        <button className="chocolate-btn">Chocolate</button>
-        <button className="vanilla-btn">Vanilla</button>
-        <button className="strawberry-btn">Strawberry</button>
+        <button
+          onClick={() => {
+            setChocVotes((prev) => prev + 1);
+          }}
+          className="chocolate-btn"
+        >
+          Chocolate
+        </button>
+        <button
+          onClick={() => {
+            setVanVotes((prev) => prev + 1);
+          }}
+          className="vanilla-btn"
+        >
+          Vanilla
+        </button>
+        <button
+          onClick={() => {
+            setStrawVotes((prev) => prev + 1);
+          }}
+          className="strawberry-btn"
+        >
+          Strawberry
+        </button>
       </section>
       <section className="results">
         <div className="chocolate">
@@ -15,30 +46,42 @@ const Votes = () => {
             <span className="bold">Chocolate: </span>
 
             <span className="percent">
-              <span className="count">2 </span> (33.3%)
+              <span className="count">{chocVotes} </span>
+              {`(${getPercent(chocVotes)}%)`}
             </span>
           </p>
-          <p className="chocolate-bar"></p>
+          <p
+            className="chocolate-bar"
+            style={{ width: `${getPercent(chocVotes)}%` }}
+          ></p>
         </div>
         <div className="vanilla">
           <p className="vanilla-result">
             <span className="bold">Vanilla: </span>
 
             <span className="percent">
-              <span className="count">2 </span> (33.3%)
+              <span className="count">{vanVotes} </span>
+              {`(${getPercent(vanVotes)}%)`}
             </span>
           </p>
-          <p className="vanilla-bar"></p>
+          <p
+            className="vanilla-bar"
+            style={{ width: `${getPercent(vanVotes)}%` }}
+          ></p>
         </div>
         <div className="strawberry">
           <p className="strawberry-result">
             <span className="bold">Strawberry: </span>
 
             <span className="percent">
-              <span className="count">2 </span> (33.3%)
+              <span className="count">{strawVotes} </span>
+              {`(${getPercent(strawVotes)}%)`}
             </span>
           </p>
-          <p className="strawberry-bar"></p>
+          <p
+            className="strawberry-bar"
+            style={{ width: `${getPercent(strawVotes)}%` }}
+          ></p>
         </div>
       </section>
     </div>
